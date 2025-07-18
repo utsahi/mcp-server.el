248dcdc3412e28e4f723190ac8c9b979519aa23c
Kishor Datar <kishordatar at g mail>
2025-07-17 19:15:31 -0700

    project git etc.

--- mcp-servers/project-mcp-server.el
+++ mcp-servers/project-mcp-server.el
@@ -1,4 +1,4 @@
-;;; project-mcp-server.el --- An in memory cache -*- lexical-binding: t; -*-
+;;; project-mcp-server.el --- Project aware mcp server -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2025 Kishor Datar
 ;; Author: Kishor Datar <kishordatar at gmail>
@@ -22,6 +22,7 @@
 (require 'eieio)
 (require 'mcp-server)
 (require 'filesys-mcp-server)
+(require 'project)
 
 (defvar project-mcp-server-last-buffer-project nil)
 (defvar project-mcp-server-set-window-project-idle-timer-duration 2)
@@ -56,17 +57,98 @@
 (cl-defmethod mcp-server-enumerate-tools ((this project-mcp-server))
   `(
     (:name "project-get-last-active-project" :description "Returns the root of the last active project if any for the current context. When calling other project related tools, if the context doesn't already contain a reference to the working directory, this tool should be used to determine the working directory."
-	   :properties ()
+	   :properties ((:name directory :type "string" :required t :description "Project root directory. Must specify full path."))
 	   :async-lambda (lambda (request arguments cb-response)
                            (mcp-server-write-tool-call-text-result
                             request
                             (if project-mcp-server-last-buffer-project (json-encode `(:root ,project-mcp-server-last-buffer-project)) (error "No project was active!"))
                             cb-response)))
+    
+    (:name "project-find-files" :description "Returns the project files."
+	   :properties ((:name directory :type "string" :required t :description "Project root directory. Must specify full path.")
+                        (:name file-names :type "string" :required t :description "One more file name substrings separated by '|'."))
+	   :async-lambda (lambda (request arguments cb-response)
+                           (let* ((directory (gethash "directory" arguments))
+                                  (file-names-arg (gethash "file-names" arguments))
+                                  (file-patterns (split-string file-names-arg "\\|" t))
+                                  (default-directory directory)
+                                  (all-files (project-files (project-current)))
+                                  (matching-files (seq-filter 
+                                                   (lambda (file)
+                                                     (seq-some (lambda (pattern)
+                                                                 (string-match-p (regexp-quote pattern) (file-name-nondirectory file)))
+                                                               file-patterns))
+                                                   all-files)))
+                             (mcp-server-write-tool-call-text-result
+                              request
+                              (json-encode `(:files ,matching-files))
+                              cb-response))))
+
+    (:name "project-git-pull-current-branch" :description "Update the repo using git pull on the current branch."
+	   :properties ((:name directory :type "string" :required t :description "Root directory of the project. Must specify full path.")
+                        (:name remote :type "string" :required nil :description "remote name. 'origin' by default."))
+	   :async-lambda (lambda (request arguments cb-response)
+                           (let* ((directory (project-mcp-server-validate-path (gethash "directory" arguments)))
+                                  (default-directory directory)
+                                  (current-branch (string-trim (shell-command-to-string "git branch --show-current")))
+                                  (command (list "git" "pull" (or (gethash "remote" arguments) "origin") current-branch "--no-tags" "--no-stat")))
+                             (filesys-mcp-server-collect-process-output
+                              command
+                              (lambda (event args)
+                                (mcp-server-write-tool-call-text-result
+                                 (plist-get args :request)
+                                 (buffer-string)
+                                 (plist-get args :cb-response)))
+                              :args (list :request request :cb-response cb-response)))))
+
+    (:name "project-git-fetch" :description "runs git fetch."
+	   :properties ((:name directory :type "string" :required t :description "Root directory of the project. Must specify full path.")
+                        (:name branch :type "string" :required t :description "branch name")
+                        (:name remote :type "string" :required nil :description "remote name. 'origin' by default."))
+	   :async-lambda (lambda (request arguments cb-response)
+                           (let* ((directory (project-mcp-server-validate-path (gethash "directory" arguments)))
+                                  (default-directory directory)
+                                  (command (list "git" "fetch" (or (gethash "remote" arguments) "origin") (gethash "branch" arguments))))
+                             (filesys-mcp-server-collect-process-output
+                              command
+                              (lambda (event args)
+                                (mcp-server-write-tool-call-text-result
+                                 (plist-get args :request)
+                                 (buffer-string)
+                                 (plist-get args :cb-response)))
+                              :args (list :request request :cb-response cb-response)))))
+
+    (:name "project-diff-pull-request" :description "Calculates merge-diff with the current branch."
+	   :properties ((:name directory :type "string" :required t :description "Root directory of the project. Must specify full path.")
+                        (:name remote :type "string" :required nil :description "remote name. 'origin' by default.")
+                        (:name branch :type "string" :required nil :description "'FETCH_HEAD' by default."))
+	   :async-lambda (lambda (request arguments cb-response)
+                           (let* ((directory (project-mcp-server-validate-path (gethash "directory" arguments)))
+                                  (default-directory directory)
+                                  (current-branch (string-trim (shell-command-to-string "git branch --show-current")))
+                                  (target-branch (or (gethash "branch" arguments) "FETCH_HEAD"))
+                                  (merge-base (string-trim (shell-command-to-string
+                                                            (format "git merge-base %s %s" 
+                                                                    (shell-quote-argument current-branch)
+                                                                    (shell-quote-argument target-branch)))))
+                                  (command (list "git"
+                                                 "merge-tree"
+                                                 merge-base
+                                                 current-branch
+                                                 target-branch)))
+                             (filesys-mcp-server-collect-process-output
+                              command
+                              (lambda (event args)
+                                (mcp-server-write-tool-call-text-result
+                                 (plist-get args :request)
+                                 (buffer-string)
+                                 (plist-get args :cb-response)))
+                              :args (list :request request :cb-response cb-response)))))
 
     (:name "project-ripgrep"
-           :description "invokes 'rg' to grep the files. Does not invoke in a shell but instead invokes the process directly."
+           :description "invokes 'rg' to grep the files. Do NOT shell-escape the arguments."
            :properties ((:name directory :type "string" :required t :description "Root search directory. Must specify full path.")
-                        (:name search-pattern :type "string" :required t :description "ripgrep regex search pattern passed to the rg executable.")
+                        (:name search-pattern :type "string" :required t :description "Escaped search regex.")
                         (:name types :type "string" :required nil :description ,(format "Optional comma-separated list of types (default: %s)" project-mcp-server-ripgrep-default-types)))
            :async-lambda
            (lambda (request arguments cb-response)
@@ -89,30 +171,42 @@
                    (plist-get args :cb-response)))
                 :args (list :request request :cb-response cb-response)))))
 
-    (:name "project-gtags-reindex" :description "reindex files using gtags"
- :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path."))
- :async-lambda (lambda (request arguments cb-response)
-                 (let* ((file-extensions '("csx" "cs" "cpp" "h" "c" "hpp" "ts" "js" "xml" "el"))
-                        (file-extensions-regex (concat "\\(" (mapconcat (lambda (ext) (concat "\\." ext "$")) file-extensions "\\)\\|\\(") "\\)"))
-                        (working-dir (project-mcp-server-validate-path (project-mcp-server-maybe-windows-path (gethash "directory" arguments))))
-                        (files (directory-files-recursively working-dir file-extensions-regex))
-                        (filtered-files (seq-filter (lambda (f) (not (string-match-p "\\\\obj\\\\" f))) files))
-                        (proc
-                         (filesys-mcp-server-collect-process-output
-                          `("gtags" "-f" "-" "--statistics" "-C" ,working-dir)
-                          (lambda (event args)
-                            (mcp-server-write-tool-call-text-result
-                             (plist-get args :request)
-                             (buffer-string)
-                             (plist-get args :cb-response)))
-                          :args (list :request request :cb-response cb-response))))
-                   (mapc (lambda (f)
-                           (process-send-string proc (project-mcp-server-maybe-windows-path f))
-                           (process-send-string proc "\n"))
-                         filtered-files)
-                   (process-send-eof proc))))
+    (:name "project-has-index" :description "Determines if the project is indexed."
+           :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path."))
+           :async-lambda (lambda (request arguments cb-response)
+                           (let* ((indexed 
+                                   (file-exists-p (file-name-concat
+                                                   (project-mcp-server-validate-path (project-mcp-server-maybe-windows-path (gethash "directory" arguments)))
+                                                   "GTAGS"))))
+                             (mcp-server-write-tool-call-text-result
+                              request
+                              (if indexed "Indexed!" "Index not found!")
+                              cb-response))))
+    
+    (:name "project-gtags-reindex" :description "reindex files using gtags. EXPENSIVE. call only if requested EXPLICITLY!"
+           :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path."))
+           :async-lambda (lambda (request arguments cb-response)
+                           (let* ((file-extensions '("csx" "cs" "cpp" "h" "c" "hpp" "ts" "js" "xml" "el"))
+                                  (file-extensions-regex (concat "\\(" (mapconcat (lambda (ext) (concat "\\." ext "$")) file-extensions "\\)\\|\\(") "\\)"))
+                                  (working-dir (project-mcp-server-validate-path (project-mcp-server-maybe-windows-path (gethash "directory" arguments))))
+                                  (files (directory-files-recursively working-dir file-extensions-regex))
+                                  (filtered-files (seq-filter (lambda (f) (not (string-match-p "\\\\obj\\\\" f))) files))
+                                  (proc
+                                   (filesys-mcp-server-collect-process-output
+                                    `("gtags" "-f" "-" "--statistics" "-C" ,working-dir)
+                                    (lambda (event args)
+                                      (mcp-server-write-tool-call-text-result
+                                       (plist-get args :request)
+                                       (buffer-string)
+                                       (plist-get args :cb-response)))
+                                    :args (list :request request :cb-response cb-response))))
+                             (mapc (lambda (f)
+                                     (process-send-string proc (project-mcp-server-maybe-windows-path f))
+                                     (process-send-string proc "\n"))
+                                   filtered-files)
+                             (process-send-eof proc))))
 
-    (:name "project-global-find-definition" :description "Find definition of symbol using GNU global. Produces grep style output."
+    (:name "project-global-find-definition" :description "Find definition of symbol using GNU global. Use if the project has index. Produces grep style output."
 	   :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path.")
                         (:name symbol :type "string" :required t :description "Symbol to lookup."))
 	   :async-lambda (lambda (request arguments cb-response)
@@ -128,7 +222,7 @@
                                  (plist-get args :cb-response)))
                               :args (list :request request :cb-response cb-response)))))
 
-    (:name "project-global-find-symbols-with-prefix" :description "Find symbols that start with the prefix using GNU global. Produces grep style output."
+    (:name "project-global-find-symbols-with-prefix" :description "Find symbols that start with the prefix using GNU global. Use if the project has index. Produces grep style output."
 	   :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path.")
                         (:name symbol :type "string" :required t :description "Symbol to lookup."))
 	   :async-lambda (lambda (request arguments cb-response)
@@ -143,7 +237,7 @@
                                  (plist-get args :cb-response)))
                               :args (list :request request :cb-response cb-response)))))
 
-    (:name "project-global-find-references" :description "Find reference to the symbol using GNU global. Produces grep style output."
+    (:name "project-global-find-references" :description "Find reference to the symbol using GNU global. Use if the project has index. Produces grep style output."
 	   :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path.")
                         (:name symbol :type "string" :required t :description "Symbol to lookup."))
 	   :async-lambda (lambda (request arguments cb-response)
@@ -160,7 +254,7 @@
                               :args (list :request request :cb-response cb-response)))))
 
     (:name "project-global-grep"
-           :description "Grep for the pattern using GNU global with -g option. Produces grep style output."
+           :description "Grep for the pattern using GNU global. Use if the index is available. with -g option. Produces grep style output."
            :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path.")
                         (:name pattern :type "string" :required t :description "Pattern to grep for."))
            :async-lambda (lambda (request arguments cb-response)
@@ -175,7 +269,7 @@
                                  (plist-get args :cb-response)))
                               :args (list :request request :cb-response cb-response)))))
 
-    (:name "project-global-find-references-from" :description "Find reference to the symbol from the given line in the reference file using GNU global. Produces grep style output.
+    (:name "project-global-find-references-from" :description "Find reference to the symbol from the given line in the reference file using GNU global. Use if the project has index. Produces grep style output.
 E.g. if token foo appears on line 10 in file.txt, this method can find all references to foo with that line and file as the starting point."
 	   :properties ((:name directory :type "string" :required t :description "Root directory to index. Must specify full path.")
                         (:name symbol :type "string" :required t :description "Symbol to lookup.")
